d993fc672140d3818561c897dd0141b69f7b932d
Björn Dahlgren <bjodah@gmail.com>
2015-11-02 20:31:42 +0100

    defer py3 support

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,5 +4,5 @@
 # or
 #   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
 python2.7 -m pytest
-python3 -m pytest --doctest-modules --pep8 --flakes $@
-python3 -m doctest README.rst
+#python3 -m pytest --doctest-modules --pep8 --flakes $@
+#python3 -m doctest README.rst
